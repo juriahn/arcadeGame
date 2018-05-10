a1af9ce6ef718888e5b04f778ef4becdf6d14603
Juri <juriahn817@gmail.com>
2018-05-09 21:51:32 -0500

    Added enemy functionality and level increment
    Added a win function that would add an enemy after user reached the end of each level. I added the level header at the top to show player what level they have reached so far. Fixed some bugs.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 // Enemies our player must avoid
+let level = 1;
 let Enemy = function(x,y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
@@ -13,17 +14,19 @@ let Enemy = function(x,y, speed) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    if (this.x === 500){
+    if (this.x >= 500){
         this.x = -100;
         this.y = Math.floor(Math.random() * 250);
+        this.speed = level >= 4 ? Math.floor(Math.random() * 4+1) : Math.floor(Math.random() * level+1);
     }     
 
     if (!(60 < difference(this.x, player.x)) && !(40 < difference(this.y, player.y)) ) {
         lose();
     }
 
-    this.x = this.x + 1;
+    this.x = this.x + this.speed;
 };
+
 
 const difference = function (a, b) { return Math.abs(a - b); }
 
@@ -50,7 +53,7 @@ Player.prototype.render = function() {
 Player.prototype.update = function() {
     if (this.y === 0 ) {
         console.log("WINNER");
-        
+        win();
     }
 }
 
@@ -103,24 +106,48 @@ Player.prototype.isOutOfBounds = function(direction) {
 // Place the player object in a variable called player
 
 
-const populateEnemies = (upperBound) => {
-    for (let i = 0; i < upperBound; i++) {
+const populateEnemies = () => {
+    allEnemies = [];
+    for (let i = 0; i < level; i++) {
         const randX = Math.floor(Math.random() * 400);
         const randY = Math.floor(Math.random() * 250);
-        allEnemies.push(new Enemy(randX, randY, 1));
+        const speed = level >= 4 ? Math.floor(Math.random() * 4+1) : Math.floor(Math.random() * level+1);
+
+        allEnemies.push(new Enemy(randX, randY, speed));
     }
 }
 
 var allEnemies = [];
-populateEnemies(5);
 var player = new Player(200, 400, 50);
+populateEnemies();
 
 
+let win = () => {
+    level++;
+    resetPlayer();
+    populateEnemies();
+    incrementLevelHeader();
+}
+
+let incrementLevelHeader = () => {
+    let levelHeaderReference = document.getElementById("headerThree");
+    levelHeaderReference.innerHTML = `level: ${level}`;
+}
 
 
 const lose = () => {
-    player = new Player(200, 400, 50);
+    level = 1;
+    resetPlayer();
+    populateEnemies();
+    incrementLevelHeader();
 }
+
+const resetPlayer = () => {
+    player = new Player(200, 400, 50);
+
+}
+
+
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
